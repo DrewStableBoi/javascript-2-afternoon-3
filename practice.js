cd3f65418fb14c8e19d0761964c3c0399b46d120
Drew Hemsley <drew@grow.com>
2019-05-11 12:46:30 -0600

    almost done with this

--- practice.js
+++ practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(array, callback) {
+  callback(array[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +49,11 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(array, callback) {
+  callback(array[array.length - 1]);
+}
+
+
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +71,10 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(n1,n2,callback) {
+  callback(n1 * n2);
+}
+
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +93,12 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(array, name, callback) {
+  if (array.includes(name)) {
+    callback(true);
+  } 
+
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,7 +119,15 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+function uniq(array, callback) {
+  const newArray = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === array[i+n]) {
+      newArray.push(array[i]);
+    }
+  }
+  callback(newArray);
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
